a61b08bcbde118b475c9f744c66cd946e11b2d74
uleh298 <uleh298@yonsei.ac.kr>
2025-08-04 17:06:03 +0900

    1541번 잃어버린 괄호

--- 김정희/0804/Main.java
+++ 김정희/0804/Main.java
@@ -1,7 +1,47 @@
-// naver.com
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("안녕 세계야");
-    }
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = bReader.readLine();
+		
+		int result = 0;
+		String number = "";
+		char cal = '+'; 
+
+		for (int i = 0; i < input.length(); i++) {
+			char ch = input.charAt(i);
+
+			if (ch != '+' && ch != '-') {
+				number += ch;
+			} else {
+				int n = Integer.parseInt(number);
+
+				if (cal == '+') {
+					result += n;
+				} else if (cal == '-') {
+					result -= n;
+				}
+		
+				if (ch == '-') {
+					cal = '-';
+				}
+
+				number = ""; 
+			}
+		}
+
+		int n = Integer.parseInt(number);
+		if (cal == '+') {
+			result += n;
+		} else if (cal == '-') {
+			result -= n;
+		}
+
+		System.out.println(result);
+	}
+
 }
