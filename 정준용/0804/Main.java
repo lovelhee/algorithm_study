25a8de92a0c4da526ee6a871832651c1714f7858
shlee <shkm1420@naver.com>
2025-08-04 10:16:03 +0900

    [그리디] 처음 접근은  -까지 괄호로묶어 위치를 저장하는 방식으로 생각했지만 그러면 메모리 할당을 너무많이 한다 그래서 다시 생ㅅ각해보니 -가 나온순간부터는 -가 몇번나오던 -이후는 모두 total값에서 빠지는 값들이라 -가 나오는 순간 boolean변수를 true로 바꿔 만약 true면 그 값들은 토탈에서 빼는식으로 변환하였다

--- 정준용/0804/Main.java
+++ 정준용/0804/Main.java
@@ -1,7 +1,52 @@
-// naver.com
+// https://www.acmicpc.net/problem/1541
+// 제목 : 잃어버린 괄호 1541
+// 알고리즘 : 그리디 알고리즘
+// 난이도 : 실버2
 
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕 세계야");
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        // 숫자와 연산자를 저장하기 위한 리스트
+        List<Integer> arr = new ArrayList<>();
+        List<Character> chr = new ArrayList<>();
+
+        String str_num = "";
+        
+        boolean isMinus = false;  // 마이너스가 나온것을 판별하기 위한변수
+        
+        // 숫자와 연산자를 따로 저장하여 숫자는 변환
+        for(int i = 0;i <str.length(); i++){
+            if('0' <= str.charAt(i) && str.charAt(i) <= '9'){
+                str_num += str.charAt(i);
+            }else{
+                arr.add( Integer.parseInt(str_num));
+                str_num = "";
+                chr.add(str.charAt(i));
+            }
+        }
+        // 마지막은 따로 저장
+        if (!str_num.equals("")) {
+            arr.add(Integer.parseInt(str_num));
+        }
+        
+        int total = arr.get(0);
+        
+        // -가 나왔는지 판별하여 -가 나온 순간부터 모두 total에서 빼기
+        for(int i = 1;i <arr.size();i++ ){
+            if(chr.get(i-1) == '-'){
+                isMinus = true;
+            }
+            if( isMinus){
+                total -= arr.get(i);
+            }else{
+                total += arr.get(i);
+            }
+        }
+        System.out.println(total);        
+
     }
 }
